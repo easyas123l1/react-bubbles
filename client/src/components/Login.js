621bee824dcd1979a86cb22e3baa9065ac962013
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-11-15 10:54:20 -0600

    login with private route

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const initialForm = {
+  username: '',
+  password: ''
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState(initialForm)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsLoggedIn(true)
+    } else {
+      setIsLoggedIn(false)
+    }
+  })
+
+  const login = e => {
+    e.preventDefault();
+    axios.post(`http://localhost:5000/api/login`, credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload)
+      setIsLoggedIn(true);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+
+  const handleChange = e => {
+    e.persist();
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+
+  }
+
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <h2>{isLoggedIn ? "LOGGED IN!" : "Please login"}</h2>
+        <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            placeholder='username'
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder='password'
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
     </>
   );
 };
 
 export default Login;
+ 
